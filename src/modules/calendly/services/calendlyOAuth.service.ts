c3fc82894ea7a8a5af49b6fb9517955a416dbf28
Patrick Otero <patrick.rocha.otero@gmail.com>
2025-09-08 21:14:35 -0300

    Fixed env variables on calendlyOAuth service

--- src/modules/calendly/services/calendlyOAuth.service.ts
+++ src/modules/calendly/services/calendlyOAuth.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@nestjs/common';
 export class InitiateOAuthService {
   async initiateOAuth(mentorId: string) {
     const params = new URLSearchParams({
-      client_id: process.env.CALENDLY_CLIENT_ID,
-      redirect_uri: process.env.CALENDLY_REDIRECT_URI,
+      client_id: process.env.SOUJUNIOR_CLIENT_ID,
+      redirect_uri: process.env.SOUJUNIOR_REDIRECT_URI,
       response_type: 'code',
       scope: 'user:read:email scheduling:read',
       state: mentorId,
